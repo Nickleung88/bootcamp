aecc50317eac718a72bdf78f174ab0de2a86994b
Nick Leung <leungchinglik@gmail.com>
2024-11-18 03:21:55 +0800

    Week6

--- demo-apie/demo-apie/src/Cat.java
+++ demo-apie/demo-apie/src/Cat.java
@@ -31,10 +31,15 @@ public String getColor(){
 
   public static void main(String[] args) {
     Cat c1 = new Cat(8, "MiuMiu", "red");
+    Cat c2 = new Cat(3,"Molly","White");
 
     System.out.println(c1.getName());
     System.out.println(c1.getAge());
     System.out.println(c1.getColor());
+
+    System.out.println(c2.getName());
+    System.out.println(c2.getAge());
+    System.out.println(c2.getColor());
 
   }
 
